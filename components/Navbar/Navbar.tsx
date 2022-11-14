7730a6d15dfa65e736988d266b7afe45415c6115
Franco Campaiola <francocampaiola@hotmail.com>
2022-11-14 20:48:09 -0300

    fix: Sticky navbar

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -3,13 +3,9 @@ import { Logo } from "./Logo"
 import { Menu } from "./Menu"
 
 export const Navbar = () => {
-
-    const bg = useColorModeValue("rgb(255, 255, 255, 0.8)", "rgba(26, 32, 44, 0.8)")
-
     return (
-        <Box position="fixed" top={0} left={0} right={0} zIndex={10} bg={bg} backdropFilter="saturate(180%) blur(5px)">
+        <Box marginTop={'-25px'} top={0} zIndex={10}>
             <Flex
-                as="nav"
                 align="center"
                 justify="space-between"
                 wrap="wrap"
--- layouts/MainLayout.tsx
+++ layouts/MainLayout.tsx
@@ -1,5 +1,5 @@
 import { FC } from 'react'
-import { Container } from '@chakra-ui/react'
+import { Box, Container, useColorModeValue } from '@chakra-ui/react'
 import { Navbar } from '../components/Navbar/Navbar';
 
 interface Props {
@@ -7,10 +7,16 @@ interface Props {
 }
 
 export const MainLayout: FC<Props> = ({ children }) => {
+    const bg = useColorModeValue("rgb(255, 255, 255, 0.8)", "rgba(26, 33, 44, 0.8)");
+    
     return (
-        <Container maxW={'5xl'} marginTop={'25px'}>
-            <Navbar />
+        <>
+            <Box w={'100%'} bg={bg} backdropFilter="saturate(180%) blur(5px)" position={'sticky'} top={0} zIndex={200}>
+                <Container as="header" maxW={'5xl'} marginTop={'25px'}>
+                    <Navbar />
+                </Container>
+            </Box>
             {children}
-        </Container>
+        </>
     )
 }
