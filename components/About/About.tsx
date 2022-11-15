747fd2b7944769e0f2fc9029d08f6250c24d6eec
Franco Campaiola <francocampaiola@hotmail.com>
2022-11-14 22:37:39 -0300

    feat: New section 'About'

--- components/About/About.tsx
+++ components/About/About.tsx
@@ -1,11 +1,42 @@
-import { Container, Heading } from '@chakra-ui/react'
+import { Button, Heading } from '@chakra-ui/react'
+import { useState } from 'react';
 import styles from './About.module.css'
+import { LongVersion } from './Versions/LongVersion';
+import { ShortVersion } from './Versions/ShortVersion';
 
 export const About = () => {
 
+  const [buttonText, setButtonText] = useState("Read long version");
+  const [showLongVersion, setShowLongVersion] = useState(false);
+
+  function handleClick() {
+    if (buttonText === "Read long version") {
+      setButtonText("Read short version");
+      setShowLongVersion(true);
+    } else {
+      setButtonText("Read long version");
+      setShowLongVersion(false);
+    }
+  }
+
   return (
-    <section id="about" className={styles.about}>
-        <Heading paddingTop={'40px'}>About me</Heading>
-    </section>
+    <>
+      <section id="about" className={styles.about}>
+        <Heading justifyItems={'start'} fontWeight={'extrabold'}>
+          About me
+        </Heading>
+        {showLongVersion && (
+          <>
+            <LongVersion />
+            <Button variant={'solid'} onClick={handleClick}>{buttonText}</Button>
+          </>
+        ) || (
+            <>
+              <ShortVersion />
+              <Button variant={'solid'} onClick={handleClick}>{buttonText}</Button>
+            </>
+          )}
+      </section>
+    </>
   )
 }
---
+++ components/About/Versions/LongVersion.tsx
@@ -0,0 +1,27 @@
+import { Code, Text } from '@chakra-ui/react'
+import React from 'react'
+
+export const LongVersion = () => {
+    return (
+        <>
+            <Text fontSize={'md'} marginTop={'30px'}>
+                I&apos;m a <Text as='b'>Software Developer</Text> with a passion for learning and creating, <Text as="kbd">focused on front-end development.</Text>
+            </Text>
+            <Text fontSize={'md'} marginTop={'20px'}>
+                Currently, I&apos;m studying <Code>Web Design and Development</Code> at <Text as='b'>Da Vinci&apos;s School.</Text>
+            </Text>
+            <Text fontSize={'md'} marginTop={'20px'}>
+                At the moment I&apos;m learning all the possible work environments of <Text as="i">web development</Text> to incorporate new skills.
+            </Text>
+            <Text fontSize={'md'} marginTop={'20px'}>
+                Since <Text as="b">August 2025</Text>, I have been working as a <Text as="em">Systems Analyst</Text> in a Pharmaceutical Laboratory located in Buenos Aires, Argentina.
+            </Text>
+            <Text fontSize={'md'} marginTop={'20px'}>
+                Since <Text as="b">August 2025</Text>, I have been working as a <Text as="em">Systems Analyst</Text> in a Pharmaceutical Laboratory located in Buenos Aires, Argentina.
+            </Text>
+            <Text fontSize={'md'} marginTop={'20px'} marginBottom={'30px'}>
+                Since <Text as="b">August 2025</Text>, I have been working as a <Text as="em">Systems Analyst</Text> in a Pharmaceutical Laboratory located in Buenos Aires, Argentina.
+            </Text>
+        </>
+    )
+}
