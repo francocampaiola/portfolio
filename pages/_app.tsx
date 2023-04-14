1b48d28b7f2403fa276fd1a139a540b0151de6d0
Franco Campaiola <francocampaiola@hotmail.com>
2023-04-14 13:40:39 -0300

    add Google Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,29 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
 import { theme } from '../themes/theme'
+import Script from 'next/script'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <Component {...pageProps } />
-    </ChakraProvider>
+    <>
+      <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-J2V0Z1T9WD" />
+      <Script
+        id='google-analytics'
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-J2V0Z1T9WD', {
+            page_path: window.location.pathname,
+                    });
+          `,
+        }}
+      />
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </>
   )
 }
